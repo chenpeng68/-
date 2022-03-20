0eb0320d31281a377da5bd7aa57449e019ec5a80
chenpeng68 <68525632+chenpeng68@users.noreply.github.com>
2022-03-20 11:00:43 +0800

    Add files via upload

--- call_back.py
+++ call_back.py
@@ -26,6 +26,7 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         self.t.start()
         self.data = str(0)
 
+        '''初始化参数'''
         self.InitUI()
         self.data_y = []
         self.data_x = []
@@ -36,38 +37,36 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         self.ptr = 0
         self.p = -1
 
-        # '''警告也采用多线程'''
-        # self.t1 = Thread(target=self.Warning())
-        # self.t1.setDaemon(True)
-        # self.t1.start()
-
         '''角度参数'''
         self.angel_para =[]
         self.angel_x_list = [0]
         self.angel_y_list = [0]
+
         '''定时器1'''
         self.timer = pg.QtCore.QTimer()
         self.timer.timeout.connect(self.update_data)
         # self.pyplotfsf画航向
         self.timer.timeout.connect(self.pyplotfsf)
         self.timer.start(500)
+
         '''定时器2'''
         self.timer1 = pg.QtCore.QTimer()
         self.timer1.timeout.connect(self.Warning)
         self.timer1.start(500)
 
-
         '''设置屏幕靠右显示'''
         self.setCenter()
 
+    '''点击定时器关闭'''
     def timeclick(self):
         self.timer1.stop()
 
+    '''定时器开启'''
     def timer_start(self):
         self.timer1.start()
 
+    '''UDP初始化'''
     def udpinit(self):
-
         ip = ""
         port = 8887
         own_addr = (ip, port)  # 接收方端口信息
@@ -76,23 +75,18 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         udp_socket.bind(own_addr)  # 绑定端口信息
         return udp_socket
 
+    '''接收对方主机发来的信号'''
     def getudpdata(self):
 
         while True:
             recv_data, other_addr = self.udp.recvfrom(1024)
-            # self.data = recv_data
             self.data = recv_data.decode("utf-8")
             # print("收到来自主机%s的消息: %s" % (other_addr, recv_data.decode("utf-8")))
 
-    '''
-    plot_rudder方向显示模块,本脚本作为UDP接收端,接收和发送的端口不能设为相同
-    '''
+    '''plot_rudder方向显示模块,对接收对方主机的数据进行处理并绘图'''
     def pyplotfsf(self):
-
-        # float_angle = float(self.data.decode("utf-8"))
         float_angle = float(self.data)
         self.angel_para.append(float_angle)
-
         '''
         math.degrees(传入参数为弧度值）# 比如math.degrees(math.PI) 结果为180（度）
         math.radians(传入参数为角度值）# 比如math.radians(180) 结果为PI
@@ -108,7 +102,6 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         # print('--angel_to_y---', self.angel_to_y[1])
         self.curve_angle_zero.setData(self.angel_x_list[:1], self.angel_y_list[:1])
         self.curve_angle.setData(self.angel_x_list[:2], self.angel_y_list[:2])
-
 
     def InitUI(self):
         '''程序启动时，首先将position_para.txt文件清空'''
@@ -136,9 +129,17 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         self.pw.setRange(xRange=[0, 2.7], yRange=[0, 4.7])
         self.pw.setMouseEnabled(x=False, y=False)
         self.curve = self.pw.plot(pen=pg.mkPen(width=5, color=(30, 144, 255)), symbolBrush=(255, 69, 0), symbolSize=10)
+        self.curve_zero = self.pw.plot(symbolBrush=(0, 255, 0), symbolSize=20, symbol='t')
         plot_display = self.findChild(QHBoxLayout, 'pyplot')
         plot_display.addWidget(self.pw)
         self.show()
+
+        ''''''
+        self.pw_present = pg.PlotWidget()
+        self.pw_present.setRange(xRange=[0, 2.7], yRange=[0, 4.7])
+        self.pw_present.setMouseEnabled(x=False, y=False)
+        self.curve_present = self.pw_present.plot(symbolBrush=(255, 0, 255), symbolSize=20)
+
 
         '''航向角模拟symbolSize=20, symbol=,, symbolBrush=(0, 255, 0)'''''
         self.pw_angle = pg.PlotWidget()
@@ -192,7 +193,7 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
             if length > 1:
                 if self.data_x[length-2] != self.data_x[length-1] or self.data_y[length-2] != self.data_y[length-1]:
                     self.p += 1
-                    print("22222", self.p)
+                    # print("22222", self.p)
                     self.data_xx.append(position_x)
                     self.data_yy.append(position_y)
                     # self.curve.setData(self.data_xx[:], self.data_yy[:])
@@ -218,6 +219,8 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
                             self.x.append(self.data_xx[self.p])
                             self.y.append(self.data_yy[self.p])
                             self.curve.setData(self.x[:], self.y[:])
+                            self.curve_present.setData(self.x[self.p], self.y[self.p])
+
 
                         else:
                             pass
@@ -229,9 +232,8 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
                         '''此处为第一个点的坐标，需要通过UDP通信协议传输给个机器人,写完退出当前UDP端口'''
                         self.x.append(self.data_xx[self.p])
                         self.y.append(self.data_yy[self.p])
-
-                        # print('-----self.x[0]-----', (self.x[0], self.y[0]))
-
+                        '''绘制起点'''
+                        self.curve_zero.setData(self.x[:], self.y[:])
                         self.curve.setData(self.x[:], self.y[:])
                         send_data = str((self.x[0], self.y[0])).encode('utf-8')
                         udp_socket.sendto(send_data, other_addr)
@@ -263,7 +265,7 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
                 此处代码的存在与否，可以认定是否设置(0,0)点为起点;
                 若为pass则视起点为开启声呐得到的第一个坐标
                 '''
-                pass
+                # pass
                 # self.data_xx.append(position_x)
                 # self.data_yy.append(position_y)
                 # self.curve.setData(self.data_xx[:], self.data_yy[:])
@@ -278,25 +280,11 @@ class mywindow(QMainWindow, Ui_mainWindow, QtWidgets.QWidget):
         if self.p >= 0:
             x = self.data_xx[self.p]
             y = self.data_yy[self.p]
-            print('---self.p--', self.p)
             if x <= 0.3 or x >= 2.4 or y <= 0.3 or y >= 4.4:
                 self.show_message(self.timeclick)
 
             else:
                 pass
-
-        # if len(self.data_xx) > 0:
-        #     print('--len----', len(self.data_xx))
-        #     x = self.data_xx[-1]
-        #     y = self.data_yyy[-1]
-        #     # x = self.x[self.p]
-        #     # y = self.y[self.p]
-        #     print('---x---', (x,y))
-        #     if x <= 0.3 or x >= 2.4 or y <= 0.3 or y >= 4.4:
-        #         QtWidgets.QMessageBox.information(self, "警告", "请操控机器人到达安全区域!!!",
-        #                                           QtWidgets.QMessageBox.Yes)
-        #     else:
-        #         pass
 
         ''' 
         读取waring.txt中的报警状态：
